3d0ee291fdbc99628176adcc7ad1f35a6b9a95d3
Anthony Barney <antbarney@outlook.com>
2021-02-01 21:26:48 -0800

    inital commit of my kb layouts

---
+++ keyboards/massdrop/alt/keymaps/antbarney/config.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#define MATRIX_IO_DELAY 40
+
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
+#define RGB_DISABLE_TIMEOUT 30000           // number of milliseconds to wait until rgb automatically turns off
+
+// Setup RGB Settings
+#define RGB_MATRIX_KEYRELEASES              // reacts to keyreleases (instead of keypresses)
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 215   // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+
+// Performance Settings
+#define RGB_MATRIX_LED_PROCESS_LIMIT 15
+#define RGB_MATRIX_LED_FLUSH_LIMIT 10
+
+// Set Startup Defaults
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE    // Sets the default mode, if none has been set
+#define RGB_MATRIX_STARTUP_SAT 255                           // Sets the default saturation value, if none has been set
+#define RGB_MATRIX_STARTUP_HUE 180                           // Sets the default hue value, if none has been set
+#define RGB_MATRIX_STARTUP_SPD 127                           // Sets the default animation speed, if none has been set
+#define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
+
+// Animations
+#define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 50
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+
+// Leaving thse ones enabled
+// #define DISABLE_RGB_MATRIX_ALPHAS_MODS
+// #define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+// #define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+// #define DISABLE_RGB_MATRIX_RAINDROPS
+// #define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+// #define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+
+// Disable the Rest/Ones I will never use.
+#define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#define DISABLE_RGB_MATRIX_BAND_SAT
+#define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#define DISABLE_RGB_MATRIX_BAND_VAL
+#define DISABLE_RGB_MATRIX_BREATHING
+#define DISABLE_RGB_MATRIX_CYCLE_ALL
+#define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+#define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+#define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#define DISABLE_RGB_MATRIX_DUAL_BEACON
+#define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#define DISABLE_RGB_MATRIX_MULTISPLASH
+#define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+#define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#define DISABLE_RGB_MATRIX_SOLID_SPLASH
+#define DISABLE_RGB_MATRIX_SPLASH
