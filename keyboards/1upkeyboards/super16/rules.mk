1da57fe84678acd3a9e9026da71274e6941d7c6d
Anthony Barney <antbarney@outlook.com>
2021-02-01 21:26:48 -0800

    inital commit of my kb layouts

--- keyboards/1upkeyboards/super16/rules.mk
+++ keyboards/1upkeyboards/super16/rules.mk
@@ -24,9 +24,9 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
-RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = WS2812
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes			# Enable RGB_MATRIX
+RGB_MATRIX_DRIVER = WS2812  # WS2812
 MIDI_ENABLE = no            # MIDI support
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
